98df9768b41a4d5f00c7951564ce1de30611827b
MedJoggins <mehdi.a.escudero@gmail.com>
2020-09-18 14:22:06 +0200

    toujours sur le header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,12 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="title">Billards World</div>
-        <button onClick className="dark">
-          Dark mode
-        </button>
+        <div className="header-content">
+          <div className="title">Billards World</div>
+          <button onClick className="dark">
+            Dark mode
+          </button>
+        </div>
       </div>
     );
   }
