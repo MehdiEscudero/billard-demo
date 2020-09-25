17215f9cc10cb6c6d9fcfc624d82ded8893192ea
MedJoggins <mehdi.a.escudero@gmail.com>
2020-09-25 15:37:04 +0200

    test input et position modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -4,23 +4,27 @@ import "../modal/modal.css";
 class Modal extends React.Component {
   render() {
     return (
-      <div className="modal-wrapper">
-        <div className="modal-header">
-          <div>Welcome to lmy first Modal</div>
-          <span className="close-modal-btn">X</span>
-        </div>
-        <div className="modal-content">
-          <div className="modal-body">
-            <h4>Modal</h4>
-            <div className="modal-p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-              nulla nobis error eaque, ullam nisi maxime dicta id deserunt,
-              corporis accusamus ad soluta ipsum. Repellendus reiciendis
-              architecto minima pariatur accusamus!
+      <div className="modal">
+        <div className="modal-wrapper">
+          <div className="modal-header">
+            <div>
+              <input type="text" name="Pseudo" />
             </div>
+            <span className="close-modal-btn">X</span>
           </div>
-          <div className="modal-footer">
-            <button className="btn-submit">Submit</button>
+          <div className="modal-content">
+            <div className="modal-body">
+              <h4>Modal</h4>
+              <div className="modal-p">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                nulla nobis error eaque, ullam nisi maxime dicta id deserunt,
+                corporis accusamus ad soluta ipsum. Repellendus reiciendis
+                architecto minima pariatur accusamus!
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn-submit">Submit</button>
+            </div>
           </div>
         </div>
       </div>
