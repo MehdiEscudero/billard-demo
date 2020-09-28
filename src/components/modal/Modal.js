357bd0684b20d1d61d98df053eb0a0b11e461974
MedJoggins <mehdi.a.escudero@gmail.com>
2020-09-28 16:10:11 +0200

    travail sur le css du modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -7,23 +7,48 @@ class Modal extends React.Component {
       <div className="modal">
         <div className="modal-wrapper">
           <div className="modal-header">
-            <div>
-              <input type="text" name="Pseudo" />
-            </div>
             <span className="close-modal-btn">X</span>
           </div>
           <div className="modal-content">
             <div className="modal-body">
-              <h4>Modal</h4>
-              <div className="modal-p">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-                nulla nobis error eaque, ullam nisi maxime dicta id deserunt,
-                corporis accusamus ad soluta ipsum. Repellendus reiciendis
-                architecto minima pariatur accusamus!
+              <span>
+                Are you a player?
+                <input type="checkbox" id="yes" name="no" checked />
+                <label for="yes">Yes</label>
+                <input type="checkbox" id="no" name="no" />
+                <label for="no">No</label>
+              </span>
+
+              <select name="billards" id="billard-select">
+                <option value="">--Choose your billard--</option>
+                <option value="francais">Français</option>
+                <option value="americain">Americain</option>
+                <option value="anglais">Anglais</option>
+                <option value="snooker">Snooker</option>
+              </select>
+
+              <input
+                type="pseudo"
+                id="pseudo"
+                name="pseudo"
+                placeholder="Pseudo"
+              ></input>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+              ></input>
+              <textarea
+                id="comment"
+                name="comment"
+                placeholder="Type here..."
+                rows="5"
+                cols="33"
+              ></textarea>
+              <div className="modal-footer">
+                <button className="btn-submit">Submit</button>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button className="btn-submit">Submit</button>
             </div>
           </div>
         </div>
