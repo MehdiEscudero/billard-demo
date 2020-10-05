80401e4115a615c3b0dc2ed946efce84c3ab54f6
MedJoggins <mehdi.a.escudero@gmail.com>
2020-10-05 14:07:41 +0200

    créa de la div intermédiaire pour close everywhere

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -72,74 +72,76 @@ class Modal extends React.Component {
     );
     return (
       <div className="modal">
-        <div className="modal-wrapper">
-          <div className="modal-header">
-            <button
-              onClick={() => this.props.handleModal(false)}
-              className="close-modal-btn"
-            >
-              X
-            </button>
-          </div>
-          <div className="modal-content">
-            <div className="modal-body">
-              <span onChange={(event) => this.handleChangePlayer(event)}>
-                Are you a player?
-                <input type="radio" id="yes" name="positive" value="yes" />
-                <label for="yes">Yes</label>
-                <input type="radio" id="no" name="negative" value="no" />
-                <label for="no">No</label>
-              </span>
+        <div onClick={() => this.props.handleModal(false)} className="forclick">
+          <div className="modal-wrapper">
+            <div className="modal-header">
+              <button
+                onClick={() => this.props.handleModal(false)}
+                className="close-modal-btn"
+              >
+                X
+              </button>
+            </div>
+            <div className="modal-content">
+              <div className="modal-body">
+                <span onChange={(event) => this.handleChangePlayer(event)}>
+                  Are you a player?
+                  <input type="radio" id="yes" name="positive" value="yes" />
+                  <label for="yes">Yes</label>
+                  <input type="radio" id="no" name="negative" value="no" />
+                  <label for="no">No</label>
+                </span>
 
-              <label>
-                Choose your billard
-                <select onChange={(event) => this.handleChangeBillard(event)}>
-                  <option value="francais">Français</option>
-                  <option value="americain">Americain</option>
-                  <option value="anglais">Anglais</option>
-                  <option value="snooker">Snooker</option>
-                </select>
-              </label>
+                <label>
+                  Choose your billard
+                  <select onChange={(event) => this.handleChangeBillard(event)}>
+                    <option value="francais">Français</option>
+                    <option value="americain">Americain</option>
+                    <option value="anglais">Anglais</option>
+                    <option value="snooker">Snooker</option>
+                  </select>
+                </label>
 
-              <input
-                onChange={(event) => this.handleChangePseudo(event)}
-                type="pseudo"
-                id="pseudo"
-                name="pseudo"
-                placeholder="Pseudo"
-              ></input>
-              <input
-                onChange={(event) => this.handleChangeEmail(event)}
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-              ></input>
-              <textarea
-                onChange={(event) => this.handleChangeComment(event)}
-                className="textarea"
-                id="comment"
-                name="comment"
-                placeholder="Type here..."
-                rows="5"
-                cols="33"
-              ></textarea>
-              <div className="modal-footer">
-                <div>
-                  <button
-                    disabled={
-                      !this.state.player ||
-                      !this.state.billard ||
-                      !this.state.pseudo ||
-                      !this.state.email ||
-                      !this.state.comment
-                    }
-                    onClick={(event) => this.handleSubmit(event)}
-                    //type="submit"
-                    className="btn-submit"
-                  >
-                    Submit
-                  </button>
+                <input
+                  onChange={(event) => this.handleChangePseudo(event)}
+                  type="pseudo"
+                  id="pseudo"
+                  name="pseudo"
+                  placeholder="Pseudo"
+                ></input>
+                <input
+                  onChange={(event) => this.handleChangeEmail(event)}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                ></input>
+                <textarea
+                  onChange={(event) => this.handleChangeComment(event)}
+                  className="textarea"
+                  id="comment"
+                  name="comment"
+                  placeholder="Type here..."
+                  rows="5"
+                  cols="33"
+                ></textarea>
+                <div className="modal-footer">
+                  <div>
+                    <button
+                      disabled={
+                        !this.state.player ||
+                        !this.state.billard ||
+                        !this.state.pseudo ||
+                        !this.state.email ||
+                        !this.state.comment
+                      }
+                      onClick={(event) => this.handleSubmit(event)}
+                      //type="submit"
+                      className="btn-submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
